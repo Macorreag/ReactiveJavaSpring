93b414495e052d82ded20a8ca00efb847b9c8030
Miller Correa <macorreag@unal.edu.co>
2022-11-16 15:18:41 -0500

    implement of suscribe for a Stream

--- src/main/java/com/bolsadeideas/springboot/reactor/app/SpringBootReactorApplication.java
+++ src/main/java/com/bolsadeideas/springboot/reactor/app/SpringBootReactorApplication.java
@@ -1,13 +1,46 @@
 package com.bolsadeideas.springboot.reactor.app;
 
+import javax.management.RuntimeErrorException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringBootReactorApplication {
+import reactor.core.publisher.Flux;
 
+@SpringBootApplication
+public class SpringBootReactorApplication implements CommandLineRunner{
+
+	private static final Logger Log = LoggerFactory.getLogger(SpringBootReactorApplication.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootReactorApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		Flux<String> nombres = Flux.just("Andres", "Pedro", "", "Diego", "Juan")
+				.doOnNext(
+						e -> {
+							if(e.isEmpty()) {
+								throw new RuntimeException("Nombres no pueden ser vacíos");
+							}else {
+								System.out.println();									
+							}
+						});
+						
+						
+		
+		
+		nombres.subscribe(
+				Log::info, //Funcion para cada elemento que llega en la susccripción 
+				error -> Log.error(error.getMessage()) // Funcion en caso de error
+				);
+		
+		
 	}
 
 }
